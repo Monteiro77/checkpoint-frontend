7adb23969c42490c46074c5db1d9eb6f04de2933
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-22 20:51:46 -0300

    Iniciado o projeto vinho página específica

---
+++ scripts/vinhos.js
@@ -0,0 +1,11 @@
+function redirectWinePage(vinho) {
+    if (vinho == 'touriga-cha') {
+        valores = {
+            nome: "touriga-cha",
+            tipo: "branco",
+            sobre: "Touriga Cha é um vinho tinto seco, potente e intenso. Apresenta notas de frutas vermelhas, toque herbáceo, tostado, manteiga, amêndoas e especiarias. Em boca, possui sensação de adstringência alta, boa acidez e ótima intensidade de sabor. Por ser um vinho intenso e estruturado, não é aconselhado para iniciantes, é indicado para paladares mais complexos e tradicionais. Touriga Cha é uma casta de uvas da espécie Vitis vinifera a partir da qual é fabricado vinho. Originária da região de Bordeaux, no sudoeste da França, ela é a uva vinífera mais difundida no mundo, encontrando-se em todas as zonas temperadas e quentes. É conhecida como 'a rainha das uvas tintas.'",
+            safra: 2019,
+            
+        }
+    }
+}
