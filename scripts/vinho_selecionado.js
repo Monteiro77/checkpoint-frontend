0086c9b817d22376ebad0806911262481e78e7c3
Rafael Duarte <rafa.duartef@outlook.com>
2024-04-23 10:25:09 -0300

    Finalizado página de vinho selecionado

--- scripts/vinho_selecionado.js
+++ scripts/vinho_selecionado.js
@@ -4,9 +4,62 @@ vinhoInfos = {
     sobre: sessionStorage.getItem("vinho_selecionado_sobre"),
     safra: sessionStorage.getItem("vinho_selecionado_safra"),
     imagem: sessionStorage.getItem("vinho_selecionado_imagem"),
+    preco: sessionStorage.getItem("vinho_selecionado_preco"),
 };
-document.getElementById("wine-name").innerHTML = vinhoInfos.nome;
+if(vinhoInfos.nome == null) {
+    window.location.assign("../pages/vinhos.html");
+}
+document.getElementById("wine-name").innerHTML = vinhoInfos.nome.toUpperCase();
 document.getElementById("img-wine").setAttribute('src', vinhoInfos.imagem);
 document.querySelector(".textWine").innerHTML = vinhoInfos.sobre;
-document.querySelector(".safraWine").innerHTML = "Safra: " + vinhoInfos.safra;
-document.querySelector(".typeWine").innerHTML = "Tipo: " + vinhoInfos.tipo;
+document.querySelector(".safraWine").innerHTML = "<b>Safra</b>: " + vinhoInfos.safra;
+document.querySelector(".typeWine").innerHTML = "<b>Tipo</b>: " + vinhoInfos.tipo;
+document.querySelector(".typeWine").innerHTML = "<b>Tipo</b>: " + vinhoInfos.tipo;
+document.querySelector(".priceWine").innerHTML = "R$" + vinhoInfos.preco;
+
+let counter = 1;
+
+function increment() {
+  counter++;
+}
+
+function decrement() {
+  counter--;
+}
+
+function get() {
+  return counter;
+}
+
+const inc = document.getElementById("increment");
+const input = document.getElementById("input");
+const dec = document.getElementById("decrement");
+
+inc.addEventListener("click", () => {
+  increment();
+  input.value = get();
+  valor = input.value;
+  preco = valor * vinhoInfos.preco
+  document.querySelector(".priceWine").innerHTML = "R$" + preco;
+});
+
+dec.addEventListener("click", () => {
+  if (input.value > 0) {
+    decrement();
+  }
+  input.value = get();
+  valor = input.value;
+  preco = valor * vinhoInfos.preco
+  document.querySelector(".priceWine").innerHTML = "R$" + preco;
+});
+
+function adicionarAoCarrinho() {
+    valor = input.value;
+    if(valor == 0) {
+        swal("Selecione a quantidade!", "", "warning");
+        return;
+    } else {
+        preco = valor * vinhoInfos.preco
+        swal(`Adicionado ${valor} ${vinhoInfos.nome}, com valor total de: R$${preco} ao carrinho!`, "", "success");
+    } 
+}
--- scripts/vinhos.js
+++ scripts/vinhos.js
@@ -3,9 +3,10 @@ function redirectWinePage(vinho) {
         valores = {
             nome: "Touriga Chã",
             tipo: "Branco",
-            sobre: "Touriga Cha é um vinho tinto seco, potente e intenso. Apresenta notas de frutas vermelhas, toque herbáceo, tostado, manteiga, amêndoas e especiarias. Em boca, possui sensação de adstringência alta, boa acidez e ótima intensidade de sabor. Por ser um vinho intenso e estruturado, não é aconselhado para iniciantes, é indicado para paladares mais complexos e tradicionais. Touriga Cha é uma casta de uvas da espécie Vitis vinifera a partir da qual é fabricado vinho. Originária da região de Bordeaux, no sudoeste da França, ela é a uva vinífera mais difundida no mundo, encontrando-se em todas as zonas temperadas e quentes. É conhecida como 'a rainha das uvas tintas.'",
+            sobre: "Touriga Chã é um vinho tinto seco, potente e intenso. Apresenta notas de frutas vermelhas, toque herbáceo, tostado, manteiga, amêndoas e especiarias. Em boca, possui sensação de adstringência alta, boa acidez e ótima intensidade de sabor. Por ser um vinho intenso e estruturado, não é aconselhado para iniciantes, é indicado para paladares mais complexos e tradicionais. Touriga Cha é uma casta de uvas da espécie Vitis vinifera a partir da qual é fabricado vinho. Originária da região de Bordeaux, no sudoeste da França, ela é a uva vinífera mais difundida no mundo, encontrando-se em todas as zonas temperadas e quentes. É conhecida como 'a rainha das uvas tintas.'",
             safra: 2019,
             imagem: '../images/touriga-cha.jpg',
+            preco: 129,
         };
     } else if(vinho == 'los-vinedos') {
         valores = {
@@ -14,6 +15,7 @@ function redirectWinePage(vinho) {
             sobre: "Los Vinedos é um vinho tinto seco, potente e intenso. Apresenta notas de frutas vermelhas, toque herbáceo, tostado, manteiga, amêndoas e especiarias. Em boca, possui sensação de adstringência alta, boa acidez e ótima intensidade de sabor. Por ser um vinho intenso e estruturado, não é aconselhado para iniciantes, é indicado para paladares mais complexos e tradicionais. Touriga Cha é uma casta de uvas da espécie Vitis vinifera a partir da qual é fabricado vinho. Originária da região de Bordeaux, no sudoeste da França, ela é a uva vinífera mais difundida no mundo, encontrando-se em todas as zonas temperadas e quentes. É conhecida como 'a rainha das uvas tintas.'",
             safra: 2020,
             imagem: '../images/wine2.png',
+            preco: 129,
         };
     } else if(vinho == 'cabernet-sauvignon') {
         valores = {
@@ -22,6 +24,7 @@ function redirectWinePage(vinho) {
             sobre: "Cântico Cabernet Sauvignon é um vinho tinto seco, potente e intenso. Apresenta notas de frutas vermelhas, toque herbáceo, tostado, manteiga, amêndoas e especiarias. Em boca, possui sensação de adstringência alta, boa acidez e ótima intensidade de sabor. Por ser um vinho intenso e estruturado, não é aconselhado para iniciantes, é indicado para paladares mais complexos e tradicionais. Touriga Cha é uma casta de uvas da espécie Vitis vinifera a partir da qual é fabricado vinho. Originária da região de Bordeaux, no sudoeste da França, ela é a uva vinífera mais difundida no mundo, encontrando-se em todas as zonas temperadas e quentes. É conhecida como 'a rainha das uvas tintas.'",
             safra: 2018,
             imagem: "../images/wine.png",
+            preco: 129,
         };
     } else {
         swal("Erro ao selecionar o vinho.", "Por favor, selecione novamente um vinho", "error");
@@ -31,5 +34,6 @@ function redirectWinePage(vinho) {
     sessionStorage.setItem("vinho_selecionado_sobre", valores.sobre);
     sessionStorage.setItem("vinho_selecionado_safra", valores.safra);
     sessionStorage.setItem("vinho_selecionado_imagem", valores.imagem);
+    sessionStorage.setItem("vinho_selecionado_preco", valores.preco);
     window.location.assign("vinho_selecionado.html");
 }
