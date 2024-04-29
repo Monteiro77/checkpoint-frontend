6767c740977ccdf7347348b490927a7bef39d34f
rafaelgbm <rafaelgasparmartins@icloud.com>
2024-04-29 10:51:17 -0300

    button favorito

--- scripts/vinho_selecionado.js
+++ scripts/vinho_selecionado.js
@@ -117,3 +117,18 @@ function adicionarAoCarrinho() {
         });
     } 
 }
+
+//BUTTON//
+
+const heart = document.querySelector(".heart");
+const animationHeart = document.querySelector(".animation-heart");
+
+heart.addEventListener('click', () =>{
+    animationHeart.classList.add('animation');
+    heart.classList.add('fill-color');
+});
+
+animationHeart.addEventListener('click', () =>{
+    animationHeart.classList.remove('animation');
+    heart.classList.remove('fill-color');
+});
