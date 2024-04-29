b8d148f639bc0b45d1e44cdaeb7346c860fbc13e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-29 20:37:40 -0300

    hotfix: page of harmonizacao.html

--- scripts/vinhos.js
+++ scripts/vinhos.js
@@ -14,7 +14,7 @@ function redirectWinePage(vinho) {
             tipo: "Tinto",
             sobre: "Los Vinedos é um vinho tinto seco, potente e intenso. Apresenta notas de frutas vermelhas, toque herbáceo, tostado, manteiga, amêndoas e especiarias. Em boca, possui sensação de adstringência alta, boa acidez e ótima intensidade de sabor. Por ser um vinho intenso e estruturado, não é aconselhado para iniciantes, é indicado para paladares mais complexos e tradicionais. Touriga Cha é uma casta de uvas da espécie Vitis vinifera a partir da qual é fabricado vinho. Originária da região de Bordeaux, no sudoeste da França, ela é a uva vinífera mais difundida no mundo, encontrando-se em todas as zonas temperadas e quentes. É conhecida como 'a rainha das uvas tintas'.",
             safra: 2020,
-            imagem: '../images/wine2.png',
+            imagem: '../images/cantico.png',
             preco: 129,
         };
     } else if(vinho == 'cabernet-sauvignon') {
@@ -23,7 +23,7 @@ function redirectWinePage(vinho) {
             tipo: "Tinto",
             sobre: "Cântico Cabernet Sauvignon é um vinho tinto seco, potente e intenso. Apresenta notas de frutas vermelhas, toque herbáceo, tostado, manteiga, amêndoas e especiarias. Em boca, possui sensação de adstringência alta, boa acidez e ótima intensidade de sabor. Por ser um vinho intenso e estruturado, não é aconselhado para iniciantes, é indicado para paladares mais complexos e tradicionais. Touriga Cha é uma casta de uvas da espécie Vitis vinifera a partir da qual é fabricado vinho. Originária da região de Bordeaux, no sudoeste da França, ela é a uva vinífera mais difundida no mundo, encontrando-se em todas as zonas temperadas e quentes. É conhecida como 'a rainha das uvas tintas'.",
             safra: 2018,
-            imagem: "../images/wine.png",
+            imagem: "../images/los-vinedos.png",
             preco: 129,
         };
     } else {
